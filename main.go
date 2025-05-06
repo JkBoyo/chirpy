d4922de8b9fd09db6d7f7d552ab3da703f54f351
JkBoyo <jkmarhefka@gmail.com>
2025-05-06 10:55:06 -0400

    healthz request

--- main.go
+++ main.go
@@ -6,11 +6,19 @@ import (
 
 func main() {
 	serveMux := http.NewServeMux()
-	handle := http.FileServer(http.Dir("./"))
-	serveMux.Handle("/", handle)
+	handle := http.StripPrefix("/app", http.FileServer(http.Dir("./")))
+	serveMux.Handle("/app/", handle)
+	serveMux.HandleFunc("/healthz", readiness)
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
 	server.ListenAndServe()
+}
+
+func readiness(rW http.ResponseWriter, req *http.Request) {
+	rW.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	rW.WriteHeader(200)
+	resp := []byte("OK")
+	rW.Write(resp)
 }
