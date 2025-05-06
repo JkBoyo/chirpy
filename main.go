5bfd283f3bdfd87e1d5d3b1f5e7bbeb73f413204
JkBoyo <jkmarhefka@gmail.com>
2025-05-06 14:44:26 -0400

    Updated paths and return html instead of plain text

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ func main() {
 	serveMux := http.NewServeMux()
 	handle := http.StripPrefix("/app", http.FileServer(http.Dir("./")))
 	serveMux.Handle("/app/", cfg.middlewareMetricsInc(handle))
-	serveMux.HandleFunc("GET /healthz", readiness)
-	serveMux.HandleFunc("GET /metrics", cfg.metrics)
-	serveMux.HandleFunc("POST /reset", cfg.reset)
+	serveMux.HandleFunc("GET /api/healthz", readiness)
+	serveMux.HandleFunc("GET /admin/metrics", cfg.metrics)
+	serveMux.HandleFunc("POST /admin/reset", cfg.reset)
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
@@ -29,9 +29,18 @@ func readiness(rW http.ResponseWriter, req *http.Request) {
 }
 
 func (cfg *apiConfig) metrics(rW http.ResponseWriter, req *http.Request) {
-	rW.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	rW.Header().Set("Content-Type", "text/html; charset=utf-8")
 	rW.WriteHeader(200)
-	respStr := fmt.Sprintf("Hits: %d", cfg.serverHits.Load())
+	respStr := fmt.Sprintf(
+		`
+		<html>
+		  <body>
+		    <h1>Welcome, Chirpy Admin</h1>
+		    <p>Chirpy has been visited %d times!</p>
+		  </body>
+		</html>
+		`,
+		cfg.serverHits.Load())
 	resp := []byte(respStr)
 	rW.Write(resp)
 }
