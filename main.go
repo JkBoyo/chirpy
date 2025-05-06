3ab9f59721724c00b906c45657dd0f592d9dc166
JkBoyo <jkmarhefka@gmail.com>
2025-05-06 13:37:48 -0400

    Add serverHits

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
+	cfg := apiConfig{}
 	serveMux := http.NewServeMux()
 	handle := http.StripPrefix("/app", http.FileServer(http.Dir("./")))
-	serveMux.Handle("/app/", handle)
+	serveMux.Handle("/app/", cfg.middlewareMetricsInc(handle))
 	serveMux.HandleFunc("/healthz", readiness)
+	serveMux.HandleFunc("/metrics", cfg.metrics)
+	serveMux.HandleFunc("/reset", cfg.reset)
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
@@ -21,4 +26,33 @@ func readiness(rW http.ResponseWriter, req *http.Request) {
 	rW.WriteHeader(200)
 	resp := []byte("OK")
 	rW.Write(resp)
+}
+
+func (cfg *apiConfig) metrics(rW http.ResponseWriter, req *http.Request) {
+	rW.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	rW.WriteHeader(200)
+	respStr := fmt.Sprintf("Hits: %d", cfg.serverHits.Load())
+	resp := []byte(respStr)
+	rW.Write(resp)
+}
+
+func (cfg *apiConfig) reset(rW http.ResponseWriter, req *http.Request) {
+	rW.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	rW.WriteHeader(200)
+	cfg.serverHits.Store(0)
+	respStr := fmt.Sprintf("Hits: %d", cfg.serverHits.Load())
+	resp := []byte(respStr)
+	rW.Write(resp)
+}
+
+type apiConfig struct {
+	serverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.serverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+
 }
